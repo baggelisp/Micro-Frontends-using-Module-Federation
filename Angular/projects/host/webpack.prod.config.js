dbc95f384470c8d46182e74b0504cd93c7adcfb6
vangelis spathonis <baggelisp.kef@hotmail.com>
2021-10-21 23:45:01 +0300

    fix for prod webpack

--- Angular/projects/host/webpack.prod.config.js
+++ Angular/projects/host/webpack.prod.config.js
@@ -1,1 +1,45 @@
-module.exports = require('./webpack.config');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, '../../tsconfig.json'),
+  ['shared-lib']);
+
+module.exports = {
+  output: {
+    uniqueName: "host",
+    publicPath: "auto"
+  },
+  optimization: {
+    runtimeChunk: false
+  },   
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      
+        remotes: {
+          "mfe1": "mfe1@https://condescending-wiles-6d014a.netlify.app/mfe1remoteEntry.js",
+          "mfe2": "mfe2@https://wizardly-chandrasekhar-fe8924.netlify.app/mfe2remoteEntry.js",
+
+        },
+
+        shared: share({
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+          ...sharedMappings.getDescriptors()
+        })
+        
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
